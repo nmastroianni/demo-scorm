ddefe0687d7c38032d827161c8781d87cce59a27
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-12-26 06:06:30 -0500

    Added 404-like message

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import CourseOverview from './lessons/CourseOverview'
 import Lesson1 from './lessons/Lesson1'
 import Lesson2 from './lessons/Lesson2'
 import { JSX, useEffect } from 'react'
+import Container from './components/Container'
+import { Button } from './components/ui/button'
 
 function App(): JSX.Element {
-  const { currentLesson } = useCourse()
+  const { currentLesson, setCurrentLesson } = useCourse()
   useEffect(() => {
     const root = window.document.documentElement
     root.classList.add('dark')
@@ -18,6 +20,22 @@ function App(): JSX.Element {
         {currentLesson === 0 && <CourseOverview key={1} />}
         {currentLesson === 1 && <Lesson1 key={2} />}
         {currentLesson === 2 && <Lesson2 key={3} />}
+        {currentLesson > 2 && (
+          <Container width="prose">
+            <p>
+              Oops. We can't seem to find the next lesson. Please let someone
+              know you've found a bug.
+            </p>
+            <Button
+              onClick={() => {
+                setCurrentLesson(currentLesson - 1)
+                localStorage.setItem('lessonProgress', `${currentLesson - 1}`)
+              }}
+            >
+              Go Back
+            </Button>
+          </Container>
+        )}
       </AnimatePresence>
     </main>
   )
